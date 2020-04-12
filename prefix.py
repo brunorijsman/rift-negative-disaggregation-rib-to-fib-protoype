e1cf95dad430703889506fd843f871a095d561e1
Bruno Rijsman <brunorijsman@gmail.com>
2020-04-12 10:22:45 -0600

    Add Prefix class

---
+++ prefix.py
@@ -0,0 +1,12 @@
+import ipaddress
+
+class Prefix:
+
+    def __init__(self, prefix_str):
+        ipv4_network = ipaddress.IPv4Network(prefix_str)
+        self._address = ipv4_network.network_address
+        self._prefix_len = ipv4_network.prefixlen
+
+    def __repr__(self):
+        return str(self._address) + '/' + str(self._prefix_len)
+        
